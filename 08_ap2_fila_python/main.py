c40b236852855a2af69164674a34a5d2b54334ea
Everton Russomanno <evertonmartins154@gmail.com>
2023-08-13 18:44:11 -0300

    Add fila em phyton AP2

---
+++ 08_ap2_fila_python/main.py
@@ -0,0 +1,30 @@
+import os
+
+sair = 0
+fila = []
+
+while sair != 1:
+    entrada = int(input('''
+        Selecione:
+         [1] Adicionar número
+         [2] Excluir número
+         [0] Sair \n 
+        '''))
+        
+    if entrada == 1:
+        recebido = input('O que deseja adicionar? ')
+        fila.append(recebido)
+        os.system('clear') or None
+        print(fila)
+    
+    if entrada == 2:
+        if not fila:
+            os.system('clear') or None
+            print("A fila está vazia.")
+        else:
+            fila.pop(0)
+            os.system('clear') or None
+            print(fila)
+        
+    if entrada == 0:
+        sair = 1
